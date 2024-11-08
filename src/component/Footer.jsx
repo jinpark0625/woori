2b222d114fba22483e5ef6e74f7c33b6fdd3b92d
jin park <jinpark0625@gmail.com>
2024-11-08 18:20:00 +0900

    remoce dark mode

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="bg-white dark:bg-gray-900 m-4">
+    <footer class="bg-white m-4">
       <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
           <a
@@ -15,7 +15,7 @@ const Footer = () => {
               alt="클럽 이미지"
             />
           </a>
-          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
             <li>
               {/* <a href="#" class="hover:underline me-4 md:me-6">
                 About
@@ -41,8 +41,8 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center ">
           {/* © 2023{" "}
           <a href="https://flowbite.com/" class="hover:underline">
             Flowbite™
